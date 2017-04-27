6c7ac3d5f15fc9641f38f69657bd1df824b400bd
Vuong Le <vle7@uncc.edu>
2017-04-27 23:20:55 +0000

    got path to work

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,7 +1,6 @@
 Given /^the following movies exist:$/ do |movies_table|
   movies_table.hashes.each do |movie|
-    Movie.create!(movie)
-
+    @movie = Movie.create(movie)
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -21,7 +21,9 @@ module NavigationHelpers
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
+    when /^the (edit|details) page for "(.*)"$/
+      movie = Movie.find_by_title($2)
+      $1 == "details" ? movie_path(movie) : edit_movie_path(movie)
     else
       begin
         page_name =~ /^the (.*) page$/
